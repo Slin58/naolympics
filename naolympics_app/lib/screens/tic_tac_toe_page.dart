88dd02621efda5f9b7449b433bb93d20c7736b03
Michael Schmitt <leahcim.s@t-online.de>
2023-08-16 12:11:08 +0200

    implemented tictactoe rewrite

--- naolympics_app/lib/screens/tic_tac_toe_page.dart
+++ naolympics_app/lib/screens/tic_tac_toe_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:naolympics_app/services/gamemodes/tictactoe/tictactoe.dart';
+
+import '../services/gamemodes/tictactoe/tictactoe_local.dart';
+import '../utils/utils.dart';
 
 class TicTacToePage extends StatefulWidget {
   const TicTacToePage({super.key});
@@ -8,97 +12,127 @@ class TicTacToePage extends StatefulWidget {
 }
 
 class TicTacToeState extends State<TicTacToePage> {
-  late List<List<String>> _board;
-  late bool _isCircleTurn;
-  late String _winner;
+  late TicTacToe ticTacToe;
 
   @override
   void initState() {
     super.initState();
-    _initializeBoard();
+    ticTacToe = TicTacToeLocal();
   }
 
-  void _initializeBoard() {
-    setState(() {
-      _board = List.generate(3, (_) => List.filled(3, ''));
-      _isCircleTurn = true;
-      _winner = "";
-    });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Tic Tac Toe'),
+        ),
+        body: _buildTicTacToeField());
   }
 
-  void _makeMove(int row, int col) {
-    if (_board[row][col] == '' && _winner == "") {
-      setState(() {
-        _board[row][col] = _isCircleTurn ? 'O' : 'X';
-        _checkWinner(row, col);
-        _isCircleTurn = !_isCircleTurn;
-      });
-    }
+  Widget _buildTicTacToeField() {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    final double smallerValue = height < width ? height : width;
+    final double fieldSize = smallerValue * 0.7;
+
+    return Column(
+      children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int row = 0; row < 3; row++)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (int col = 0; col < 3; col++)
+                      GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              TicTacToeWinner winner =
+                                  ticTacToe.makeMove(row, col);
+                              if (winner != TicTacToeWinner.ongoing) {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return showWinner(winner);
+                                  },
+                                );
+                              }
+                            });
+                          },
+                          child: _buildTicTacToeCell(row, col, fieldSize)),
+                  ],
+                ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
-  void _checkWinner(int row, int col) {
-    String currentSymbol = _board[row][col];
-    bool won = false;
-    bool isBoardFull = true;
+  Container _buildTicTacToeCell(int row, int col, double fieldSize) {
+    return Container(
+      width: fieldSize / 3,
+      height: fieldSize / 3,
+      decoration: const BoxDecoration(
+        border: Border(
+          right: BorderSide(color: Colors.black),
+          left: BorderSide(color: Colors.black),
+          bottom: BorderSide(color: Colors.black),
+          top: BorderSide(color: Colors.black),
+        ),
+      ),
+      child: Center(child: setIcon(row, col)),
+    );
+  }
 
-    // Check row
-    if (_board[row][0] == currentSymbol &&
-        _board[row][1] == currentSymbol &&
-        _board[row][2] == currentSymbol) {
-      won = true;
-    }
+  AlertDialog showWinner(TicTacToeWinner winner) {
+    const double iconSize = 40;
+    const double buttonWidth = 130;
+    Icon winnerIcon;
+    String winnerText = "Winner: ";
 
-    // Check column
-    if (_board[0][col] == currentSymbol &&
-        _board[1][col] == currentSymbol &&
-        _board[2][col] == currentSymbol) {
-      won = true;
+    if (winner == TicTacToeWinner.o) {
+      winnerIcon = getCircleIcon(iconSize);
+    } else if (winner == TicTacToeWinner.x) {
+      winnerIcon = getCrossIcon(iconSize);
+    } else {
+      winnerIcon =
+          const Icon(Icons.bolt_outlined, size: iconSize, color: Colors.amber);
+      winnerText = "It's a tie!";
     }
-
-    // Check diagonal (top-left to bottom-right)
-    if (_board[0][0] == currentSymbol &&
-        _board[1][1] == currentSymbol &&
-        _board[2][2] == currentSymbol) {
-      won = true;
-    }
-
-    // Check diagonal (top-right to bottom-left)
-    if (_board[0][2] == currentSymbol &&
-        _board[1][1] == currentSymbol &&
-        _board[2][0] == currentSymbol) {
-      won = true;
-    }
-
-    // check for tie
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        if (_board[i][j] == "") {
-          isBoardFull = false;
-          break;
-        }
-      }
-      if (!isBoardFull) {
-        break;
-      }
-    }
-
-    if (won) {
-      setState(() {
-        _winner = currentSymbol;
-      });
-    } else if (isBoardFull) {
-      setState(() {
-        _winner = "tie";
-      });
-    }
+    return AlertDialog(
+      title: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text(winnerText), const SizedBox(width: 8.0), winnerIcon],
+        ),
+      ),
+      content: Row(mainAxisSize: MainAxisSize.min, children: [
+        UIUtils.getBorderedTextButton(() {
+          Navigator.of(context).pop();
+          Navigator.of(context).pop();
+        }, Icons.arrow_back, 'Go Back', Theme.of(context).primaryColor,
+            buttonWidth),
+        const SizedBox(width: buttonWidth / 4),
+        UIUtils.getBorderedTextButton(() {
+          Navigator.of(context).pop();
+          setState(() {
+            ticTacToe.init();
+          });
+        }, Icons.refresh, 'Reset', Theme.of(context).primaryColor, buttonWidth),
+      ]),
+    );
   }
 
   Icon? setIcon(int row, int col) {
     const double iconSize = 120;
+    TicTacToeFieldValues fieldValue = ticTacToe.playField[row][col];
 
-    if (_board[row][col] == 'O') {
+    if (fieldValue == TicTacToeFieldValues.o) {
       return getCircleIcon(iconSize);
-    } else if (_board[row][col] == 'X') {
+    } else if (fieldValue == TicTacToeFieldValues.x) {
       return getCrossIcon(iconSize);
     } else {
       return null;
@@ -111,115 +145,5 @@ class TicTacToeState extends State<TicTacToePage> {
 
   Icon getCrossIcon(double size) {
     return Icon(Icons.close, size: size, color: Colors.black);
-  }
-
-  AlertDialog showWinner() {
-    const double iconSize = 40;
-    Icon winnerIcon;
-    String winnerText;
-
-    if (_winner == 'O') {
-      winnerIcon = getCircleIcon(iconSize);
-      winnerText = "Winner: ";
-    } else if (_winner == 'X') {
-      winnerIcon = getCrossIcon(iconSize);
-      winnerText = "Winner:";
-    } else {
-      winnerIcon =
-          const Icon(Icons.bolt_outlined, size: iconSize, color: Colors.amber);
-      winnerText = "It's a tie!";
-    }
-
-    return AlertDialog(
-      title: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(winnerText), const SizedBox(width: 8.0), winnerIcon],
-        ),
-      ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Theme.of(context).primaryColor),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: TextButton.icon(
-              onPressed: () {
-                Navigator.of(context).pop();
-                _initializeBoard();
-              },
-              icon: Icon(Icons.refresh, color: Theme.of(context).primaryColor),
-              label: Text(
-                'Reset',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 18.0,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
-    final double width = MediaQuery.of(context).size.width;
-    final double smallerValue = height < width ? height : width;
-    final double fieldSize = smallerValue * 0.7;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tic Tac Toe'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                for (int row = 0; row < 3; row++)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      for (int col = 0; col < 3; col++)
-                        GestureDetector(
-                          onTap: () {
-                            _makeMove(row, col);
-                            if (_winner != "") {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return showWinner();
-                                },
-                              );
-                            }
-                          },
-                          child: Container(
-                            width: fieldSize / 3,
-                            height: fieldSize / 3,
-                            decoration: const BoxDecoration(
-                              border: Border(
-                                right: BorderSide(color: Colors.black),
-                                left: BorderSide(color: Colors.black),
-                                bottom: BorderSide(color: Colors.black),
-                                top: BorderSide(color: Colors.black),
-                              ),
-                            ),
-                            child: Center(child: setIcon(row, col)),
-                          ),
-                        ),
-                    ],
-                  ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
--- naolympics_app/lib/utils/utils.dart
+++ naolympics_app/lib/utils/utils.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
-class DebugAnnotation extends StatelessWidget {
-  final Widget child;
+class UIUtils {
+  static Container getBorderedTextButton(void Function()? onPressed,
+      IconData iconData, String text, Color color, double width) {
+    return Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: color),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        width: width,
+        child: TextButton.icon(
+          onPressed: onPressed,
+          icon: Icon(iconData, color: color),
+          label: Text(
+            text,
+            style: TextStyle(
+              color: color,
+              fontSize: 18.0,
+            ),
+          ),
+        ));
+  }
 
-  const DebugAnnotation({super.key, required this.child});
-
-  @override
-  Widget build(BuildContext context) {
+  static Container debugBorder(Widget child) {
     return Container(
       decoration: BoxDecoration(
         border: Border.all(color: Colors.red, width: 2.0),
