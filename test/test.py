e9fd2dbb39935fad88e624c3d046076c81236c5e
Nils Göbel <nils.goebel@student.fhws.de>
2023-08-22 15:54:10 +0200

    implemented player2 functionality

--- test/test.py
+++ test/test.py
@@ -17,15 +17,66 @@ robotIP = "10.30.4.13"
 PORT = 9559
 
 
-def game_done():
-    i = random.randint(0, 1)
-    if i == 0:
-        return True
-    else:
-        return False
+def choose_game_by_voice():  # returns "tictactoe" or "4 gewinnt" depending on what you say -> after 20 seconds without recognized word returns None
+    asr = ALProxy("ALSpeechRecognition", robotIP, PORT)
+    memory = ALProxy("ALMemory", robotIP, PORT)
+    tts = ALProxy("ALTextToSpeech", robotIP, PORT)
+
+    def on_word_recognized(value):
+        print(value)
+        recognized_word = value[0]
+        print("Recognized:", recognized_word)
+
+        if recognized_word == "Hello":
+            tts.say("Hello!")
+
+    asr.setLanguage("German")
+
+    vocabulary = ["tictactoe", "Tictactoe", "Gewinnt", "gewinnt", "vier", "Vier"]
+    asr.setVocabulary(vocabulary, True)
+    asr.setParameter("Sensitivity", 0.3)
+
+    asr.subscribe("MySpeechRecognition")
+    memory.subscribeToEvent("WordRecognized", "MySpeechRecognition", "on_word_recognized")
+
+    empty_data = ['', 0]
+    memory.insertData("WordRecognized", empty_data)
+
+    # asr.callback("WordRecognized", "Hello", std::string subscriberIdentifier)
+    data = memory.getData("WordRecognized")
+
+    print ("Speech recognition engine started")
+    tts.say("Was möchtest du spielen?")
+
+    counter = 0
+    while True:
+        if 'Tictactoe' in data[0] or 'tictactoe' in data[0]:
+            tts.say("Okay lass uns Tictactoe spielen!")
+            memory.unsubscribeToEvent("WordRecognized", "MySpeechRecognition")
+            asr.unsubscribe("MySpeechRecognition")
+            return "tictactoe"
+        elif 'gewinnt' in data[0] or 'vier' in data[0] and 'Gewinnt' in data[0] and 'Vier' in data[0]:
+            tts.say("Okay lass uns 4 gewinnt spielen!")
+            memory.unsubscribeToEvent("WordRecognized", "MySpeechRecognition")
+            asr.unsubscribe("MySpeechRecognition")
+            return "4 gewinnt"
+        elif counter > 20:
+            tts.say("Ich hab dich leider nicht verstanden")
+            memory.unsubscribeToEvent("WordRecognized", "MySpeechRecognition")
+            asr.unsubscribe("MySpeechRecognition")
+            return None
+        counter += 1
+        time.sleep(1)
+
+        data = memory.getData("WordRecognized")
 
 
-def play_against_itself(robotIP, PORT):
+def choose_game_by_buttons():
+    print("not implemented")  # todo
+    return None
+
+
+def play_tictactoe_against_itself(robotIP, PORT):
     circleTurn = True
     img = vision.get_image_from_nao(ip=robotIP, port=PORT)
     field = vision.detect_tictactoe_state(img, debug=[], minRadius=65, maxRadius=85, acc_thresh=15,
@@ -57,9 +108,40 @@ def play_against_itself(robotIP, PORT):
         movementControl.clickTicTacToe(robotIP, PORT, result)
 
 
-def play_tictactoe_against_opponent(robotIP, PORT, difficulty="m"):
+def play_connect_four_against_itself(robotIP, PORT):    # todo test after vision is done
+    circleTurn = True
+    img = vision.get_image_from_nao(ip=robotIP, port=PORT)
+    field = vision.detect_connect_four_state(img, debug=[], minRadius=40, maxRadius=55, acc_thresh=10,
+                                                 circle_distance=120, canny_upper_thresh=30, dilate_iterations=4,
+                                                 erode_iterations=1, gaussian_kernel_size=13)
+    result = None
+    if field is not None:
+        result = connect_four_tactic.nextMove(field, signOwn='R', signOpponent='Y', signEmpty='-', mistake_factor=0)
+        print(result)
+        movementControl.clickConnectFour(robotIP, PORT, result)
+    else:
+        print("field none!")
+
+    while True:
+        time.sleep(6.0)
+        circleTurn = not circleTurn
+        img = vision.get_image_from_nao(ip=robotIP, port=PORT)
+        field = vision.detect_connect_four_state(img, debug=[], minRadius=40, maxRadius=55, acc_thresh=10,
+                                                 circle_distance=120, canny_upper_thresh=30, dilate_iterations=4,
+                                                 erode_iterations=1, gaussian_kernel_size=13)
+
+        if circleTurn and field is not None:
+            result = connect_four_tactic.nextMove(field, signOwn='Y', signOpponent='R', signEmpty='-', mistake_factor=0)
+        elif field is not None:
+            result = connect_four_tactic.nextMove(field, signOwn='R', signOpponent='Y', signEmpty='-', mistake_factor=0)
+        else:
+            break
+        print(field)
+        movementControl.clickConnectFour(robotIP, PORT, result)
+
+
+def play_tictactoe_against_opponent_player1(robotIP, PORT, difficulty="m"):
     field_after_move = []
-    own_turn = True
     while True:
 
         img = vision.get_image_from_nao(ip=robotIP, port=PORT)
@@ -68,7 +150,7 @@ def play_tictactoe_against_opponent(robotIP, PORT, difficulty="m"):
                                               gaussian_kernel_size=9)
         time.sleep(0.5)
         field_none_counter = 0
-        while field is None:  # while field is None mit counter bis 3
+        while field is None:
             field_none_counter += 1
             time.sleep(0.5)
             img = vision.get_image_from_nao(ip=robotIP, port=PORT)
@@ -87,7 +169,6 @@ def play_tictactoe_against_opponent(robotIP, PORT, difficulty="m"):
 
         # difficulty = 'e' -> easy ,'m' -> medium,'h' -> hard,'i' -> impossible
         result = tictactoeTactic.nextMove(field, signOwn='O', signOpponent='X', signEmpty='-', difficulty=difficulty)
-        # result = tictactoeTactic.nextMove(field, signOwn='X', signOpponent='O', signEmpty='-', difficulty='i')
         field_after_move = field
         print(result)
 
@@ -113,7 +194,65 @@ def play_tictactoe_against_opponent(robotIP, PORT, difficulty="m"):
         movementControl.clickTicTacToe(robotIP, PORT, result)
 
 
-def play_connect_four_against_opponent(robotIP, PORT):
+def play_tictactoe_against_opponent_player2(robotIP, PORT, difficulty="m"):
+    field_after_move = \
+        [['-', '-', '-'],
+         ['-', '-', '-'],
+         ['-', '-', '-']]
+    while True:
+
+        img = vision.get_image_from_nao(ip=robotIP, port=PORT)
+        field = vision.detect_tictactoe_state(img, minRadius=65, maxRadius=85, acc_thresh=15,
+                                              canny_upper_thresh=25, dilate_iterations=8, erode_iterations=4,
+                                              gaussian_kernel_size=9)
+        time.sleep(0.5)
+        field_none_counter = 0
+        while field is None:
+            field_none_counter += 1
+            time.sleep(0.25)
+            img = vision.get_image_from_nao(ip=robotIP, port=PORT)
+            field = vision.detect_tictactoe_state(img, minRadius=65, maxRadius=85, acc_thresh=15,
+                                                  canny_upper_thresh=25, dilate_iterations=8, erode_iterations=4,
+                                                  gaussian_kernel_size=9)
+            if field is None and field_none_counter > 5:
+                print("Game over")
+                # todo implement if won celebration else disappointment
+                return
+            
+        if field == field_after_move:
+            time.sleep(0.5)
+            continue
+
+        print(field)
+
+        # difficulty = 'e' -> easy ,'m' -> medium,'h' -> hard,'i' -> impossible
+        result = tictactoeTactic.nextMove(field, signOwn='X', signOpponent='O', signEmpty='-', difficulty=difficulty)
+        field_after_move = field
+        print(result)
+
+        if result == 0:
+            field_after_move[0][0] = 'X'
+        elif result == 1:
+            field_after_move[0][1] = 'X'
+        elif result == 2:
+            field_after_move[0][2] = 'X'
+        elif result == 3:
+            field_after_move[1][0] = 'X'
+        elif result == 4:
+            field_after_move[1][1] = 'X'
+        elif result == 5:
+            field_after_move[1][2] = 'X'
+        elif result == 6:
+            field_after_move[2][0] = 'X'
+        elif result == 7:
+            field_after_move[2][1] = 'X'
+        else:
+            field_after_move[2][2] = 'X'
+
+        movementControl.clickTicTacToe(robotIP, PORT, result)
+
+
+def play_connect_four_against_opponent_player1(robotIP, PORT, mistake_factor=0):
     field_after_move = []
     while True:
         img = vision.get_image_from_nao(ip=robotIP, port=PORT)
@@ -147,12 +286,61 @@ def play_connect_four_against_opponent(robotIP, PORT):
                                                  gaussian_kernel_size=13)
         #
         # # difficulty = 'e' -> easy ,'m' -> medium,'h' -> hard,'i' -> impossible
-        result = connect_four_tactic.nextMove(field, signOwn='Y', signOpponent='R', signEmpty='-', mistake_factor=0)
+        result = connect_four_tactic.nextMove(field, signOwn='Y', signOpponent='R', signEmpty='-', mistake_factor=mistake_factor)
         field_after_move = field
         field_after_move = connect_four_tactic.setPointY(field_after_move, -1, result)
         print(result)
 
         movementControl.clickConnectFour(robotIP, PORT, result)
+
+
+def play_connect_four_against_opponent_player2(robotIP, PORT, mistake_factor=0):
+    field_after_move = \
+        [['-', '-', '-', '-', '-', '-', '-'],
+         ['-', '-', '-', '-', '-', '-', '-'],
+         ['-', '-', '-', '-', '-', '-', '-'],
+         ['-', '-', '-', '-', '-', '-', '-'],
+         ['-', '-', '-', '-', '-', '-', '-'],
+         ['-', '-', '-', '-', '-', '-', '-']]
+    while True:
+        img = vision.get_image_from_nao(ip=robotIP, port=PORT)
+        field = vision.detect_connect_four_state(img, debug=[], minRadius=40, maxRadius=55, acc_thresh=10,
+                                                 circle_distance=120, canny_upper_thresh=30, dilate_iterations=4,
+                                                 erode_iterations=1, gaussian_kernel_size=13)
+        field_none_counter = 0
+        while field is None:  # while field is None mit counter bis 3
+            field_none_counter += 1
+            time.sleep(0.5)
+            img = vision.get_image_from_nao(ip=robotIP, port=PORT)
+            field = vision.detect_connect_four_state(img, minRadius=40, maxRadius=55, acc_thresh=10,
+                                                     circle_distance=120, canny_upper_thresh=30, dilate_iterations=4,
+                                                     erode_iterations=1, gaussian_kernel_size=13)
+            if field is None and field_none_counter > 5:
+                print("Game over")
+                # todo implement if won celebration else disappointment
+                return
+        if field == field_after_move:
+            time.sleep(1)
+            continue
+        else:
+            print("field", field)
+            print("field_after_move", field_after_move)
+            print("comparison not successful")
+
+        print(field)
+        img = vision.get_image_from_nao(ip=robotIP, port=PORT)
+        field = vision.detect_connect_four_state(img, minRadius=40, maxRadius=55, acc_thresh=10, circle_distance=120,
+                                                 canny_upper_thresh=30, dilate_iterations=4, erode_iterations=1,
+                                                 gaussian_kernel_size=13)
+        #
+        # # difficulty = 'e' -> easy ,'m' -> medium,'h' -> hard,'i' -> impossible
+        result = connect_four_tactic.nextMove(field, signOwn='R', signOpponent='Y', signEmpty='-', mistake_factor=mistake_factor)
+        field_after_move = field
+        field_after_move = connect_four_tactic.setPointR(field_after_move, -1, result)
+        print(result)
+
+        movementControl.clickConnectFour(robotIP, PORT, result)
+
 
 if __name__ == "__main__":
     # after startup of nao
@@ -166,63 +354,12 @@ if __name__ == "__main__":
 
     # start positions
     # movementControl.startPosition(robotIP, PORT)
-    # vision.record_image_from_nao("../230822_faulty_recognition_c4.png", robotIP, PORT)
-    # play_tictactoe_against_opponent(robotIP, PORT, difficulty='h')
-    play_connect_four_against_opponent(robotIP,   PORT)
-    # img = vision.get_image_from_nao(ip=robotIP, port=PORT)
-    # field = vision.detect_connect_four_state(img, debug=[], minRadius=30, maxRadius=55, acc_thresh=10,
-    #                                          circle_distance=120, canny_upper_thresh=30, dilate_iterations=4,
-    #                                          erode_iterations=1, gaussian_kernel_size=13, white_thresh=100)
-    # print(field)
-    # result = connect_four_tactic.nextMove(field, signOwn='Y', signOpponent='R', signEmpty='-', mistake_factor=0)
-    # # result = tictactoeTactic.nextMove(field, signOwn='X', signOpponent='O', signEmpty='-', difficulty='i')
-    # # field_after_move = field
-    # print(result)
-    #
-    # movementControl.clickConnectFour(robotIP, PORT, result)
-    # vision.detect_connect_four_state(img,
 
-    # field = vision.detect_connect_four_state(img,debug=[1,2,3,4,5], minRadius=30, maxRadius=55,acc_thresh=10,circle_distance=120, canny_upper_thresh=30, dilate_iterations=4, erode_iterations=1, gaussian_kernel_size=13, white_thresh=100)
-    # def on_word_recognized(value):
-    #     recognized_word = value[0]
-    #     print("Recognized:", recognized_word)
-    #
-    #     if recognized_word == "hallo":
-    #         tts.say("Hello!")
-    #
-    #
-    # tts = ALProxy("ALTextToSpeech", robotIP, PORT)
-    # asr = ALProxy("ALSpeechRecognition", robotIP, PORT)
-    # memory = ALProxy("ALMemory", robotIP, PORT)
 
-    # memory.unsubscribeToEvent("WordRecognized", "on_word_recognized")
-    # asr.unsubscribe("MySpeechRecognition")
+    # play_tictactoe_against_itself(robotIP, PORT)
+    # play_tictactoe_against_opponent_player1(robotIP, PORT, difficulty='h')
+    # play_tictactoe_against_opponent_player2(robotIP, PORT, difficulty='h')
+    #play_connect_four_against_itself(robotIP, PORT)
+    # play_connect_four_against_opponent_player1(robotIP, PORT, mistake_factor = 0)
+    # play_connect_four_against_opponent_player2(robotIP, PORT, mistake_factor = 0)
 
-    # asr.setLanguage("German")
-    #
-    # # Example: Adds "yes", "no" and "please" to the vocabulary (without wordspotting)
-    # vocabulary = ["Ja", "hallo"]
-    # asr.setVocabulary(vocabulary, False)
-    #
-    # # Start the speech recognition engine with user Test_ASR
-    # memory.subscribeToEvent("WordRecognized", "MySpeechRecognition", "on_word_recognized")
-    #
-    # print 'Speech recognition engine started'
-
-    # tts.say("Möchten Sie Tic-Tac-Toe gegen mich spielen?")
-#
-#     try:
-#         while True:
-#             pass
-#     except KeyboardInterrupt:
-#         # Clean up
-#         asr.unsubscribe("MySpeechRecognition")
-#         memory.unsubscribeToEvent("WordRecognized", "MySpeechRecognition")
-#
-#
-# """    time.sleep(5)
-#     if asr.callback("Test_ASR", "Ja", ):
-#         tts.say("Juhu lass uns spielen")
-#     else:
-#         tts.say("Okay dann ein andernmal")
-#     asr.unsubscribe("Test_ASR")"""
