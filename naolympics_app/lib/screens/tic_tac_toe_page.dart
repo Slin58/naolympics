23f35ef8aefab8c465655665830f9b504b2c1653
Michael Schmitt <leahcim.s@t-online.de>
2023-09-11 15:44:59 +0200

    fixed navigator issue for host in tic_tac_toe_page.dart

--- naolympics_app/lib/screens/tic_tac_toe_page.dart
+++ naolympics_app/lib/screens/tic_tac_toe_page.dart
@@ -158,7 +158,6 @@ class TicTacToeState extends State<TicTacToePage> {
         (ticTacToe as TicTacToeMultiplayer).handleGoBack();
       }
       Navigator.pop(alertContext);
-      Navigator.pop(alertContext);
       Navigator.pop(context);
     }
   }
