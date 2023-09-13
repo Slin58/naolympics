d0c5f703795985d15d4263e7ed924aad3403b735
Michael Schmitt <leahcim.s@t-online.de>
2023-09-13 10:38:04 +0200

    moved logger

--- naolympics_app/lib/main.dart
+++ naolympics_app/lib/main.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-import "package:logging/logging.dart";
+import "package:naolympics_app/logger/logger.dart";
 import "package:naolympics_app/screens/connect_four_page.dart";
 import "package:naolympics_app/screens/game_selection/game_selection_multiplayer.dart";
 import "package:naolympics_app/screens/home_page.dart";
@@ -7,15 +7,8 @@ import "package:naolympics_app/screens/tic_tac_toe_page.dart";
 import "package:naolympics_app/services/routing/route_observer/observer_utils.dart";
 
 void main() {
-  Logger.root.level = Level.INFO; // defaults to Level.INFO
-  Logger.root.onRecord.listen((record) {
-    String baseMessage = "${record.time} ${record.level.name} '${record.loggerName}':\t\t${record.message}";
-    if(record.level == Level.SEVERE) {
-      baseMessage += "\n${record.error}\n${record.stackTrace}";
-    }
-    // ignore: avoid_print
-    print(baseMessage);
-  });
+  LoggerConfig.setLoggerConfig();
+
   runApp(MaterialApp(
     home: const MyApp(),
     navigatorObservers: [ObserverUtils.getRouteObserver()],
